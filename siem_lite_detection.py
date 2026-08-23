bde4b17b9a860dd96fd7df441ce7047f2a8df072
Diego_dona <dominguezn0709@gmail.com>
2026-08-23 14:46:02 -0600

    Temporary Changes

--- siem_lite_detection.py
+++ siem_lite_detection.py
@@ -4,6 +4,31 @@ import logging
 import time
 import boto3
 
+# Define the action weights for different AWS actions. These weights can be used to prioritize or score events based on their severity or importance.
+ACTION_WEIGHTS = {
+    'StopLogging': 400,
+    'DeleteTrail': 400,
+    'DisableKey': 400,
+    'ScheduleKeyDeletion': 400,
+    'UpdateTrail': 240,
+    'DeleteSecurityGroup': 240,
+    'AuthorizeSecurityGroupIngress': 200,
+    'CreateSecurityGroup': 160,
+    'RevokeSecurityGroupIngress': 120,
+    'AuthorizeSecurityGroupEgress': 120,
+    'RevokeSecurityGroupEgress': 80,
+}
+
+def get_severity(score):
+    if score >= 600:
+        return "CRITICO"
+    elif score >= 400:
+        return "ALTO"
+    elif score >= 200:
+        return "MEDIO"
+    elif score >= 1:
+        return "BAJO"
+
 # Declaring the DynamoDB resource
 dynamodb = boto3.resource('dynamodb')
 
@@ -35,16 +60,28 @@ def lambda_handler(event, context):
                     'userIdentity': userARN,
                     'sourceIPAddress': source_ip
                 },
-                UpdateExpression = "ADD #cnt :increment SET #t = :ttl_value",
+                UpdateExpression = (
+                    '#gC = if_not_exists(#gC, :increment) + :increment, '
+                    '#t = :ttl_value, '
+                    # Update the action count for the specific event name. If the action count does not exist, it initializes it to 1 and then increments it by 1.  
+                    '#aC.#eN.#c = if_not_exists(#aC.#eN.#c, :one) + :one'
+                ),
                 ExpressionAttributeNames = {
-                    '#cnt': 'counter',
-                    '#t': 'ttl'
+                    '#eN': event_name,
+                    '#aC': 'actionCounts',
+                    '#gC': 'globalCounts',
+                    '#c': 'count',
+                    '#t': 'ttl',
                 },
                 ExpressionAttributeValues = {
-                    ':increment': 1,
+                    # Increment the global count for the user and source IP by the weight of the action. If the action is not defined in ACTION_WEIGHTS, it defaults to 0.
+                    ':increment': ACTION_WEIGHTS.get(event_name, 0),
+                    ':one': 1,
                     ':ttl_value': int(time.time()) + (15 * 60)
                 },
             )
+
+
 
     except Exception as e:
         logger.error(f"Error in lambda_handler: {str(e)}")
